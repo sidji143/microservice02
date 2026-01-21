40655934f3d6245128592dd034ea8d958ebf5e8b
sidji143 <siddharthojha624@gmail.com>
2026-01-22 01:49:53 +0530

    Initial commit

---
+++ src/main/java/com/quiz02/microservices02/Microservices02Application.java
@@ -0,0 +1,13 @@
+package com.quiz02.microservices02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservices02Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/quiz02/microservices02/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.quiz02.microservices02;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Microservices02Application.class);
+	}
+
+}
